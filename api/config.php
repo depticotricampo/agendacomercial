0dd0ec9909af6b54132d53d321fe86c593c33514
depticotricampo <cotricampoagendatec@gmail.com>
2025-01-24 15:53:24 -0300

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,17 +1,15 @@
 <?php
-// Configuração da conexão com o banco de dados PostgreSQL
-$host = 'ep-spring-butterfly-a5f6zfko-pooler.us-east-2.aws.neon.tech';
-$database = 'neondb';
-$user = 'neondb_owner';
-$password = 'npg_y3fzF1kdcTxM';
 
-// Monta a string de conexão
-$dsn = "pgsql:host=$host;dbname=$database;sslmode=require";
+$host = $_ENV['PG_HOST'];
+$port = $_ENV['PG_PORT'];
+$db = $_ENV['PG_DB'];
+$user = $_ENV['PG_USER'];
+$password = $_ENV['PG_PASSWORD'];
+
+$dsn = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require";
 
 try {
-    // Tenta estabelecer a conexão
     $conexao = new PDO($dsn, $user, $password);
-    // Define o modo de erro do PDO para exceção
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Conexão bem-sucedida!";
 } catch (PDOException $e) {
