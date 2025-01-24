3c1b1f9e1f203dfb0c76de961a20bb999dfb80ef
depticotricampo <cotricampoagendatec@gmail.com>
2025-01-24 16:13:45 -0300

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,14 +1,3 @@
 <?php
-$servername = "ep-spring-butterfly-a5f6zfko-pooler.us-east-2.aws.neon.tech";
-$username = "neondb_owner"; // seu nome de usuário do MySQL
-$password = "npg_y3fzF1kdcTxM"; // sua senha do MySQL
-$dbname = "neondb"; // nome do seu banco de dados
-
-// Cria a conexão
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// Verifica a conexão
-if (!$conn) {
-    die("failed: " . mysqli_connect_error());
-}
+psql 'postgresql://neondb_owner:npg_y3fzF1kdcTxM@ep-billowing-pond-a5p02o8t-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require'
 ?>
