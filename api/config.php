6d0f96c85f28b653fe35869f99c0eba716bd9ef5
depticotricampo <cotricampoagendatec@gmail.com>
2025-01-24 16:50:34 -0300

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,25 +1,15 @@
 <?php
 // Configuração da conexão com o banco de dados PostgreSQL
-$host = 'ep-spring-butterfly-a5f6zfko-pooler.us-east-2.aws.neon.tech';
-$database = 'neondb';
-$user = 'neondb_owner';
-$password = 'npg_y3fzF1kdcTxM';
-
-// Monta a string de conexão
-$conn_string = "host=$host dbname=$database user=$user password=$password sslmode=require";
+$dbConnectionString = "postgresql://neondb_owner:npg_y3fzF1kdcTxM@ep-spring-butterfly-a5f6zfko-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require&options=endpoint%3Dep-spring-butterfly-a5f6zfko-pooler";
 
 // Tenta estabelecer a conexão
-$conexao = pg_connect($conn_string);
+$conexao = pg_connect($dbConnectionString);
 
 // Verifica se a conexão foi bem-sucedida
 if (!$conexao) {
     die("Erro na conexão: " . pg_last_error());
 }
 
-echo "Conexão bem-sucedida!";
-
 // Não se esqueça de fechar a conexão quando terminar
 // pg_close($conexao);
 ?>
-?>
-
