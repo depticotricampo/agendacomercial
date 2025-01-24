94a2c2ca1bde9329320d211ba9b1fd3fbbe7b25c
depticotricampo <cotricampoagendatec@gmail.com>
2025-01-24 16:18:04 -0300

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,3 +1,4 @@
 <?php
-$connectionString = 'postgresql://neondb_owner:npg_y3fzF1kdcTxM@ep-spring-butterfly-a5f6zfko-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require';?>
+$connectionString = 'postgresql://neondb_owner:npg_y3fzF1kdcTxM@ep-spring-butterfly-a5f6zfko-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require';
 ?>
+
